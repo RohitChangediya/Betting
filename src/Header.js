e018a87735953cd81e9d9d5e1fe66aaddf9ba883
ethorse <snipertech@protonmail.com>
2017-10-23 22:07:58 -0700

    contract updated

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import ethorsejson from './ETHorse.json';
-
+import {makeHref} from 'react-router';
 
 
 
@@ -42,16 +42,21 @@ export default class Header extends Component {
           <Navbar light expand="md">
             <NavbarBrand href="/"><h3>Ethorse</h3></NavbarBrand>
               <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink>
+                    <Button color="link"><a href={address_link} rel="noopener noreferrer" target="_blank">{ethorsejson.address}</a></Button>
+                  </NavLink>
+                </NavItem>
               <NavItem>
                 <NavLink><Button color="link" onClick={this.toggle}>Information</Button></NavLink>
               </NavItem>
               </Nav>
           </Navbar>
-        
+
         <Modal isOpen={this.state.modal} toggle={this.toggle} >
           <ModalHeader toggle={this.toggle}>Information</ModalHeader>
           <ModalBody>
-              Info page Bet on a coin and win against other bettors with ETHORSE Smart Contract
+              Bet on a coin and win against other bettors with ETHORSE Smart Contract
               <ul>
                 <li>Simply choose a winner for the next three days among BTC, ETH and LTC.</li>
                 <li>Enter the amount you are willing to bet (only Ether) from a browser with Metamask extension, Geth or Mist.</li>
@@ -60,7 +65,6 @@ export default class Header extends Component {
                 <li>Price pulled from Coinmarketcap.com API through Oraclize.it at the beginning and end of the bet period</li>
                 <li>Maximum bet amount 1 ETH and minimum 0.1 ETH Questions and Feedback welcome</li>
               </ul>
-              Link to open source smart contract code: <a href={address_link} rel="noopener noreferrer" target="_blank">{ethorsejson.address}}</a>
           </ModalBody>
           <ModalFooter>
             <Button color="primary" onClick={this.toggle}>Ok</Button>
