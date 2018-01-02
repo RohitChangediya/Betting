014fdb38dd24813a54364c50d7884a5c400b2b52
ethorse <snipertech@protonmail.com>
2018-01-02 05:48:34 -0700

    Disable placebet when betting is not open

--- src/Result.jsx
+++ src/Result.jsx
@@ -72,7 +72,7 @@ export default class Result extends Component{
   {
     if(this.state.winner==="")
     return(
-      <span style={{position:'relative',fontSize:'20px'}} className="float-left">
+      <span style={{position:'relative',fontSize:'30px'}} className="float-left">
           Live&nbsp;
         <sup ><i className="fa fa-circle" aria-hidden="true" style={{"color":"green","fontSize":"15px"}}></i></sup>
       </span>
