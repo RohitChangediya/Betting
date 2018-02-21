90f624a037808ec19db4986f1a00619a7fe8a0c7
starbuoy <starbuoy@protonmail.com>
2018-02-21 16:40:31 -0700

    Metamask installation background color changed

--- src/Warning.jsx
+++ src/Warning.jsx
@@ -6,7 +6,7 @@ export default class Warning extends React.Component{
   render(){
 
     return (<div>
-      <Jumbotron style={{ 'textAlign': 'center'}} fluid>
+      <Jumbotron style={{ 'textAlign': 'center','backgroundColor':'#262f4a'}} fluid>
       <Container>
       You do not seem to have Metamask extension installed. Please install from <a href="https://metamask.io/" target="_blank" rel="noopener noreferrer"> metamask.io</a>
     </Container>
